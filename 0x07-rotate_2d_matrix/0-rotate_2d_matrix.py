bdd952d8614fe77cfe5106cc8bf3cf10da9da9ce
10thcode <attabadukwabena@gmail.com>
2024-03-11 16:50:55 +0000

    Add 0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Define rotate_2d_matrix function.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate an array by 90 degrees.
+    """
+    for i in range(len(matrix)):
+        matrix[i].reverse()
+
+    for row in range(len(matrix)):
+        for col in range(row, len(matrix)):
+            matrix[row][col] = matrix[col][row]
+            matrix[col][row] = matrix[row][col]
+
+    for i in range(len(matrix)):
+        matrix[counter].reverse()
+
+    matrix.reverse()
